28e28e99c790426f419bf401c5c3a9ffb9b9a460
nitest <noemail@ni.com>
2017-01-12 00:34:02 -0600

    streaming data to host

--- GPU_FFT/GPU_FFT.cu
+++ GPU_FFT/GPU_FFT.cu
@@ -5,10 +5,10 @@
 * FlexRIO device. In this case, the FlexRIO module is creating a simulated CW
 * tone and can perform Additive White Gaussian Noise (AWGN) and/or a Finite
 * Impulse Response (FIR) Low-Pass Filter (LPF) on the generated signal. The
-* signal is then sent to the GPU where a parallelized FFT takes place and some
-* logarithmic conversion to calculate the power spectrum of the signal. This 
-* resulting signal is then sent to the host machine for data logging and or
-* plotting.
+* signal is then sent to the GPU where a parallelized FFT takes place using 
+* NVIDIA's CUFFT library and some logarithmic conversion to calculate the 
+* power spectrum of the signal. This resulting signal is then piped to a file
+* and/or a GNUplot host application for data logging and plotting.
 *
 * For more information on NI FPGA functions, see the NI FPGA Interface C API 
 * Help. For more information on NVIDIA CUDA functions and operation, see the
@@ -18,9 +18,17 @@
 * Author:       John Gentile
 */
 
+// includes- system
+#include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 #include <sys/time.h>
 #include <sys/times.h>
+
+// includes- project
+#include <cuda_runtime.h>
+#include <cufft.h>
+#include <cufftXt.h>
 #include "NiFpga_FPGA_Main.h"
 
 #define NX              (128*8192) //4096 (64k pages)
@@ -52,6 +60,7 @@ int main(int argc, char **argv)
   // Configure P2P FIFO between FlexRIO and GPU using NVIDIA GPU Direct
   // CHECKSTAT(NiFpga_ConfigureFifoBuffer(session, NiFpga_FPGA_Main_TargetToHostFifoI32_T2HDMAFIFO, (uint64_t)gpu_mem, NX*NFRAMES, NULL, NiFpga_DmaBufferType_NvidiaGpuDirectRdma)); 
   
+  CHECKSTAT(NiFpga_ConfigureFifo(session, NiFpga_FPGA_Main_TargetToHostFifoI32_T2HDMAFIFO, (size_t)10000));
   
   // Set RMS Noise value of AWGN Algorithm on FlexRIO (out of i16 full scale)
   NiFpga_WriteU16(session, NiFpga_FPGA_Main_ControlU16_RMSNoise, 2048);
@@ -60,6 +69,17 @@ int main(int argc, char **argv)
   NiFpga_WriteBool(session, NiFpga_FPGA_Main_ControlBool_aReset, NiFpga_True);
   NiFpga_WriteBool(session, NiFpga_FPGA_Main_ControlBool_aReset, NiFpga_False);
 
+  int32_t data [1000];
+  CHECKSTAT(NiFpga_ReadFifoI32(session, NiFpga_FPGA_Main_TargetToHostFifoI32_T2HDMAFIFO, data, (size_t)1000, 5000, NULL));
+
+  FILE *f = fopen("SimSignal.dat", "w");
+  for(int i=0; i<1000; i++)
+  {
+    if (i==0)
+      fprintf(f, "# X Y\n");
+    fprintf(f, "%d %d\n", i, data[i]);
+  }
+  fclose(f);
 
   // Close NI FPGA References; must be last NiFpga calls
   printf("Stopping NI FPGA...\n");
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # FlexRIO-P2P-GPU
 POC for utilizing peer-to-peer data streams between a National Instruments FlexRIO device and a NVIDIA GPU
+
+https://developer.nvidia.com/gpudirect
+http://www.gnuplot.info/docs_5.0/gnuplot.pdf
+https://developer.nvidia.com/cuda-zone
+https://nitalk.jiveon.com/groups/flexrio/projects/dma-to-gpu-intern-project
