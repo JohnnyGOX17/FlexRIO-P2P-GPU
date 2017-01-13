72b476782d274e4846ab2a2f29356c0e3eb8d1a2
nitest <noemail@ni.com>
2017-01-12 22:48:45 -0600

    update plot calls

--- GPU_FFT/plot/DLandPlot.sh
+++ GPU_FFT/plot/DLandPlot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-scp nitest@flexlinux:/home/nitest/FlexRIO-P2P-GPU/GPU_FFT/SimSignal.dat /home/jgentile/Downloads/
+scp nitest@flexlinux:/home/nitest/FlexRIO-P2P-GPU/GPU_FFT/SimSignal.dat ./
 gnuplot -p plt 
